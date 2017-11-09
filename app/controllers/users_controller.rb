2dc53d3d253e69ca0628c46dcd2bfda287bb67c4
om mishra <om.mishra@ajatus.co.in>
2017-11-09 11:40:09 +0000

    chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
