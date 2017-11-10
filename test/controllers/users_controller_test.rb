b78d7dd4df1cd00330d329432a61e0f64cd5779a
om mishra <om.mishra@ajatus.co.in>
2017-11-10 10:45:49 +0000

    chapter 10

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
+  end
   test "should get new" do
     get signup_path
     get users_new_url
@@ -23,5 +27,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
 
